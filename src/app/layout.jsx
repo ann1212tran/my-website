fd2f1180b75fcceb41757f4f3acee5b84c43c3e8
Anh <anhminh121204@icloud.com>
2025-02-19 09:58:21 +0000

    Specify subsets for Inter font in layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,17 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import NavBar from " ~/components/navigation/nav-bar";
+import NavBar from "~/components/navigation/nav-bar";
 import { ReactNode } from "react";
 import Header from "../components/header";
 import Footer from '../components/Footer';
 import { Inter } from "next/font/google";
 
-// import { AuthProvider } from "./Providers";
-
 // Load the Inter font with the 'swap' display strategy
-const inter = Inter ({ 
-  substs: ["latin"], display: "swap",
+const inter = Inter({ 
+  subsets: ["latin"], // Specify the subsets you need
+  display: "swap",
   preload: true,
-})
-
-
+});
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,8 +27,7 @@ const geistMono = localFont({
 export const metadata = {
   title: "My Website",
   description: "beginner to profolio"
-  }
-
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -39,19 +35,14 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
-        <Header/>
-        
+        <Header />
         <NavBar />
         <div className="flex flex-col min-h-screen">
-        <div className={inter.className}>
-      {children}
-    </div>
-        
-        
+          <div className={inter.className}>
+            {children}
+          </div>
         </div>
         <Footer />
-        
       </body>
     </html>
   );
